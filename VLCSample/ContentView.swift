c4e137362e8515ec512771d4b59f21f76a045b7f
Alessandro Oliva <alessandro.oli@hotmail.it>
2025-07-23 15:33:00 +0200

    Update ContentView.swift

--- VLCSample/ContentView.swift
+++ VLCSample/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                 if showAVPlayerButton {
                     AVPlayerButton(
                         edge: edge,
-                        useSampleFile: true
+                        useSampleFile: false
                     )
                 }
             }
